dee9181d07493f2ffcd95e7278fa00aaa38f2bc0
andreapepsii <andreagarciaf123@gmail.com>
2018-05-12 18:46:19 +0200

    Andrea second commit

---
+++ Game Jam/Assets/Andrea/Scripts/RotateEarth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateEarth : MonoBehaviour {
+
+    public Transform from;
+    public Transform to;
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+  
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0,transform.rotation.y+30, transform.rotation.eulerAngles.z+90));
+        }
+	}
+}
