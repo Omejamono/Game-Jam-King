8f05466b88a3f83bafbf7b4521688e4288c2bfa2
Dualsix <Munta92@gmail.com>
2018-05-13 01:43:33 +0200

    juego perfecto
    sdasdas

---
+++ Game Jam/Assets/Carlos/Assets/Juego_mostrar/aparecer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aparecer : MonoBehaviour {
+    //public variables
+    public GameObject limite_sup;
+    public GameObject limite_inf;
+
+    //private variables
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "middle")
+        {
+            limite_inf.SetActive(true);
+            limite_sup.SetActive(true);
+        }
+    }
+}
--- Game Jam/Assets/Carlos/Assets/Juego_mostrar/control.cs
+++ Game Jam/Assets/Carlos/Assets/Juego_mostrar/control.cs
@@ -4,38 +4,48 @@ using UnityEngine;
 
 public class control : MonoBehaviour {
     //public variables
-    public GameObject[] max;
-    public GameObject[] min;
-    public GameObject[] middle;
-    public int pos;
-
+    public int dificultad;
+    public bool can_inf;
+    public bool can_sup;
 
     //private variables
     private Vector3 aux;
     private Vector3 move;
+    private float vel;
     // Use this for initialization
     void Start () {
-        aux = new Vector3(0.0f, 0.0f, 0.0f);
-
+        aux = new Vector3(0.0f, transform.position.y, 0.0f);
+        can_inf = true;
+        can_sup = true;
+        if (dificultad == 1)
+        {
+            vel = 0.2f;
+        }
+        if (dificultad == 2)
+        {
+            vel = 0.3f;
+        }
+        if (dificultad == 3)
+        {
+            vel = 0.4f;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
-            if (pos >= max.Length)
-            {
-                pos = 0;
-            }
-            float moveVertical = Input.GetAxis("Vertical");
-            if ((aux.y + moveVertical * 0.3f) < 2 && (aux.y + moveVertical * 0.3f) > -5)
-            {
-                aux.y += moveVertical * 0.3f;
-            }
-            if (((transform.position.y > -5) || (moveVertical > 0)) && ((transform.position.y < 2) || (moveVertical < 0)))
-            {
+           float moveVertical = Input.GetAxis("Vertical");
+           //if ((aux.y + moveVertical * vel) < 2 && (aux.y + moveVertical * vel) > -5)
+           //{
+           //aux.y += moveVertical * vel;
+           //}
+           if (((can_inf) || (moveVertical > 0)) && ((can_sup) || (moveVertical < 0)))
+           {
+                aux.y += moveVertical * vel;
                 transform.position = new Vector3(transform.position.x, aux.y, transform.position.z);
-            }
+                can_inf = true;
+                can_sup = true;
+           }
         
     }
 
