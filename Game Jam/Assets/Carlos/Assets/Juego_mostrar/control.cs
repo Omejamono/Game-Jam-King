66aa82f23514246277642dfbfc2c8e3802675e53
Dualsix <Munta92@gmail.com>
2018-05-12 15:11:01 +0200

    ,etadatos
    dasdas

--- Game Jam/Assets/Carlos/Assets/Juego_mostrar/control.cs
+++ Game Jam/Assets/Carlos/Assets/Juego_mostrar/control.cs
@@ -21,7 +21,6 @@ public class control : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(pos);
         if (pos >= max.Length)
         {
             pos = 0;
