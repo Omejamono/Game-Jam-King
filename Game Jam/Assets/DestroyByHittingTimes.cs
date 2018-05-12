561edace95c3c695b1be9a29ce01ac2a1996a5a7
Omejamono <Omejamono@users.noreply.github.com>
2018-05-12 14:16:53 +0200

    Miguel Comit

---
+++ Game Jam/Assets/DestroyByHittingTimes.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByHittingTimes : MonoBehaviour {
+    public string tag;
+    public Vector3 inverseVelocity;
+    public bool hitted = false;
+    public float timeRA;
+
+    public float life; 
+
+    private float stopRA;
+
+
+    private void Update()
+    {
+        if (hitted)
+        {
+            if (stopRA < Time.time) { 
+                GetComponent<Rigidbody>().velocity += -inverseVelocity;
+                hitted = false; 
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == tag)
+        {
+            life--;
+
+            if (!hitted)
+            {
+                hitted = true;
+                stopRA = Time.time + timeRA;
+                GetComponent<Rigidbody>().velocity += inverseVelocity;
+            }
+
+            if (life <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Game Jam/Assets/playerRazonar.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Boundary
+{
+    public float yMin, yMax;
+}
+
+public class playerRazonar : MonoBehaviour {
+    public float speed;
+    public Boundary boundary;
+
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private float nextFire;
+
+    private Vector3 inicialPosition;
+
+    private void Start()
+    {
+        inicialPosition = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        float moveVertical = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(0.0f, moveVertical, 0.0f);
+
+        GetComponent<Rigidbody>().velocity = movement * speed;
+
+        transform.position = new Vector3
+        (
+            inicialPosition.x,
+            Mathf.Clamp (transform.position.y, boundary.yMin, boundary.yMax),
+            inicialPosition.z
+        ); 
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Jump") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+    }
+}
