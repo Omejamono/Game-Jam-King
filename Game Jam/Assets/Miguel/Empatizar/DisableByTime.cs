ba7a2103c8210b2893cfc970a4dca75dab4b721b
Omejamono <Omejamono@users.noreply.github.com>
2018-05-12 19:00:41 +0200

    Miguel

---
+++ Game Jam/Assets/Miguel/Empatizar/DisableByTime.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableByTime : MonoBehaviour {
+    public float lifetime;
+
+    private float timer;
+    private bool created = true; 
+
+    private void Update()
+    {
+        if (created)
+        {
+            timer = Time.time + lifetime;
+            created = false; 
+        }
+        else { 
+            if (timer < Time.time)
+            {
+                created = true; 
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Game Jam/Assets/Miguel/Empatizar/empatizarController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class empatizarController : MonoBehaviour {
+    public int numCombi;
+    public int[] arrayFlechas;
+    public int numFlechas;
+    public GameObject[] flechas;
+    public GameObject game;
+    public GameObject Heart;
+
+    private int aux = 0;
+
+    public bool simon = true;
+
+    private float timer;
+    public float nextFlecha;
+
+    public bool levelup = false;
+
+    private void Start()
+    {
+        fillArray();
+        timer = nextFlecha + Time.time;
+    }
+
+    private void Update()
+    {
+        if (simon)
+        {
+            doSimon();
+        }
+        else
+        {
+            if (levelup)
+            {
+                nextWave();
+                levelup = false; 
+            }
+        }
+    }
+
+    private void fillArray()
+    {
+        arrayFlechas = new int[numFlechas];
+        for (int i = 0; i < numFlechas; i++)
+        {
+            arrayFlechas[i] = Random.Range(0, 4);
+        }
+    }
+
+    private void doSimon() {
+        if (arrayFlechas.Length > aux) {
+            if (timer < Time.time)
+            {
+                for (int i = 0; i < flechas.Length; i++) {
+                    flechas[i].SetActive(false);
+                } 
+                flechas[arrayFlechas[aux]].SetActive(true);
+                aux++;
+
+                timer = nextFlecha + Time.time;
+            }
+        }
+        else
+        {
+            simon = false;
+            Heart.SetActive(true);
+            GetComponent<empatizarUser>().enabled = true;
+        }
+    }
+
+    private void nextWave()
+    {
+        numCombi--;
+        if (numCombi == 0)
+        {
+            Destroy(game);
+        }
+        aux = 0;
+        numFlechas++;
+        fillArray();
+        simon = true; 
+    }
+}
