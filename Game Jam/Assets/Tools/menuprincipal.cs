28806d6f08e4c4108c52183bc764e2f460f7ab7d
Dualsix <Munta92@gmail.com>
2018-05-13 05:12:43 +0200

    menu inicial
    asdasdas

---
+++ Game Jam/Assets/Tools/menuprincipal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuprincipal : MonoBehaviour {
+    public 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void jugar()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void salir()
+    {
+        Application.Quit();
+    }
+}
